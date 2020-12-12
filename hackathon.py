9ed336a96d16c157429242af3f42b817e3b08638
Neel Salvi <ndsalvi02@gmail.com>
2020-12-12 15:48:58 -0500

    Added a plot to it.

--- hackathon.py
+++ hackathon.py
@@ -1,5 +1,13 @@
-#imports
 import investpy
+import matplotlib.pyplot as plt
+import pandas
+import json
+
+dates = []
+closes= []
+
+
+
 
 print('What stock would you like to review?')
 stockName = input()
@@ -10,11 +18,21 @@ startDate = input()
 print('Input a date you would like to review the stock to. (dd/mm/yyyy).')
 endDate = input() 
 
-historicalData = investpy.get_stock_historical_data(stock=stockName,
-                                        country=countryName,
-                                        from_date=startDate,
-                                        to_date=endDate)
+historicalData = investpy.get_stock_historical_data(stock=stockName,country=countryName,from_date=startDate,to_date=endDate)
+#historicalData = investpy.get_stock_historical_data('TSLA','United States','10/09/2016','10/10/2016', as_json=True)
 
 
-print(historicalData.tail(10))
+LastTenDays = json.loads(historicalData)
 
+for i in range(0,10):
+	dates.append(LastTenDays['historical'][i]['date'])
+	closes.append(LastTenDays['historical'][i]['close'])
+
+
+'''
+for i in range(LastTenDays):
+	dates = LastTenDays.values[i][0]
+	closes = LastTenDays.values[i][1]
+'''
+plt.plot(dates, closes)
+plt.show()
